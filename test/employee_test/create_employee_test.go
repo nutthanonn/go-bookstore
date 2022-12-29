eb8ad60042f28211f272d44200c5810d5a1dfb7a
nutthanon <nutthanon.tho@gmail.com>
2022-12-29 18:41:30 +0700

    feat(testing): unit testing - employee/customer/book

---
+++ test/employee_test/create_employee_test.go
@@ -0,0 +1,39 @@
+package employeetest
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+	utils "github.com/gofiber/fiber/v2/utils"
+	"github.com/nutthanonn/go-clean-architecture/api/infrastructure/datastore"
+	"github.com/nutthanonn/go-clean-architecture/api/routers"
+	"github.com/nutthanonn/go-clean-architecture/pkg/registry"
+)
+
+func Test_CreateEmployee(t *testing.T) {
+	db := datastore.NewDB()
+	defer func() {
+		sqlDB, err := db.DB()
+
+		if err != nil {
+			panic(err)
+		}
+
+		sqlDB.Close()
+	}()
+
+	app := fiber.New()
+	api := app.Group("/api")
+	r := registry.NewRegistry(db)
+	routers.EmployeeRouter(api, r.NewAppController())
+
+	t.Run("FAIL CREATE EMPLOYEE", func(t *testing.T) {
+		req := httptest.NewRequest("POST", "/api/employee", nil)
+		req.Header.Set("Content-Type", "application/json")
+		res, err := app.Test(req)
+
+		utils.AssertEqual(t, nil, err, "app.Test(req)")
+		utils.AssertEqual(t, fiber.StatusBadRequest, res.StatusCode, "Status code")
+	})
+}
